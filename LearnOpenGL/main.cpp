9bc9c3dd9ea4b172ad323230094a3cb5dd1588b8
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2020-04-18 17:49:53 +0500

    fix comments

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,4 +1,4 @@
-#define GLEW_STATIC
+п»ї#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -8,6 +8,8 @@
 #include <Windows.h>
 
 using namespace std;
+
+// EBO - element buffer objects
 
 const char* _readFile(const char* path) {
 	ifstream::pos_type size;
@@ -125,7 +127,7 @@ void _draw(GLuint vao, GLuint shaderProgram) {
 	glUseProgram(shaderProgram); 
 	glBindVertexArray(vao); 
 
-	//без EBO
+	//without EBO
 	//glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -167,14 +169,14 @@ int main() {
 	glViewport(0, 0, width, height);
 		
 	GLfloat vertices[] = {
-		0.5f,  0.5f, 0.0f,  // Верхний правый угол
-		0.5f, -0.5f, 0.0f,  // Нижний правый угол
-		-0.5f, -0.5f, 0.0f,  // Нижний левый угол
-		-0.5f,  0.5f, 0.0f   // Верхний левый угол
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left
 	};
-	GLuint indices[] = {  // Помните, что мы начинаем с 0!
-		0, 1, 3,   // Первый треугольник
-		1, 2, 3    // Второй треугольник
+	GLuint indices[] = {  // start from 0
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 	
 	//Vertex Buffer Objects
@@ -191,26 +193,26 @@ int main() {
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// 1. Привязываем VAO
+	// 1. bind VAO
 	glBindVertexArray(VAO);
-	// 2. Копируем наш массив вершин в буфер для OpenGL
+	// 2. copy vertex array into OpenGL buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
-	// 3. Копируем наши индексы в в буфер для OpenGL
+	// 3. copy indexes into OpenGL buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
-	// 4. Устанавливаем указатели на вершинные атрибуты 
+	// 4. set pointers at vertex attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0); 
-	// 5. Отвязываем VAO
+	// 5. unbind VAO
 	glBindVertexArray(0); 
 		
 	GLuint shaderProgram = _shaders();
 
-	//режим wireframe
+	//wireframe mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	//нормальный режим
+	//normal mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	_log("Commencing");
